f3f9e2a7522317546d5f6acb7dc96a324926830b
Josh Kilgore <joshkilgore@Joshs-MacBook-Pro.local>
2016-02-29 20:57:37 -0600

    fixed functions taking them outside of for loop and made it print tree to console

--- quiz.js
+++ quiz.js
@@ -1,34 +1,60 @@
-var tree = { };
+//variables declared******
 
-var treeHeight = document.getElementById("height");
-  console.log(treeHeight);
-var addCharacter = document.getElementById("char"); 
-  console.log(addCharacter);
 var buildButton = document.getElementById("buildButton");
-var myTree = document.getElementById("myTree");
-var div = ' ';
-// 
+var clearButton = document.getElementById("clear");
+var treeElements = document.getElementById("treeHolder");
+var line = "";
+
+//**********FUNCTION TO BUILD TREE TIED TO VALIDATE FUNCTION TO ENSURE NO TREE WILL BUILD IF BOTH BOXES AREN'T FILLED IN WITH VALUES*****
 
 
-//
+function buildTree() {
+  var treeStuff = validateInputs();
+ tree(treeStuff) }
 
-buildButton.addEventListener("click", buildTree);
-function buildTree(event){
-  event.preventDefault();
-  tree.height = treeHeight.value;
-  tree.char = addCharacter.value;
-  for (var i = 0; i <= tree.height; i++) 
-    for (var j = 0; j<= tree.height; j++){
+//TREE OBJECT INPUT INTO TREE FUNCTION, RUNS FOR LOOPS SO THAT THE FORMULA FOR BUILDING THE TREE SHAPE IS EXECUTED AND IS ADJUSTED BASED ON HEIGHT INPUT*****
+  function tree(myTreeObject){
+    for (var i = 1; i <= myTreeObject.height; i ++) {
+       line=""; 
+      for (var j = 0; j <= myTreeObject.height-i-1; j++) {
+        line += " ";
+      }
+    for (var j = 1; j<= 2*i - 1; j++) {
+      line += myTreeObject.character;
+    }
+    //LINE IS ENTERED TO BREAK THE LOOP AND HAVE THE FORUMLA CONTINUE ON THE NEXT CONSOLE LINE TO MAKE TREE SHAPE*****
+      console.log(line);
+    }
+    
+  }
+
+//*****************FUNCTION VALIDATES INPUT FROM USER TO ENSURE THERE ARE VALUES IN BOTH INPUT BOXES IN HTML*******
+
+function validateInputs() {
+  if (((document.getElementById("height").value).length == 0) || ((document.getElementById("char").value).length == 0)) {
+    alert("Please enter both a height and a character!")
+    // return false;
+  }
+
+  // IF THERE IS A VALUE IN BOTH BOXES NO ALERT IS SHOWN AND THE TREE IS READY TO BE BUILT AND WILL BE RETURNED
+
+  else {
+    var treeHeight = document.getElementById("height").value;
+    var treeChar = document.getElementById("char").value;
+  // var n = treeChar.toString();
+    var treeObject = {
+    height: treeHeight,
+    character: treeChar
+  }
+  return treeObject;
+  }
+  
+
+
+
+
 }
-  for (var i = 0; i >= tree.height; i++) {
-  }
-    myTree.innerHTML += tree.char;
-    myTree.innerHTML += "<br>";
-  }
-console.log("I clicked it", tree);
+//submitButton.addEventListener("click", validateInputs);
+buildButton.addEventListener("click", buildTree);
 
-  //   treeHeight = makeProductString(myArray[i]);
-  //   var productElement = document.getElementById(myId);
-  //   productElement.innerHTML += productString;
-  //   console.log(productElement);
- //};
+
